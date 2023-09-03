7e78d476bffaaaaefa14b61f0603b96a254acc8d
shahnaj parven <shahnajritu21@gmail.com>
2023-09-03 16:52:26 +0600

    changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import  express from "express";
-const app = express();
+import cors from 'cors';
+
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import fileUpload from "express-fileupload";
@@ -9,31 +10,32 @@ import AppRoutes from './routes/index.js';
 
 import errorMidleware from "./middleware/error.js";
 
+const app = express();
+
+const options = [
+    cors({ origin: true, credentials: true }),
+    express.json({ limit: '30mb' }),
+    cookieParser(),
+    bodyParser.urlencoded({ extended: true }),
+    fileUpload()
+];
+
+app.use("*",options);
+
+
 //config 
 if (process.env.NODE_ENV !== "PRODUCTION") {
     dotenv.config({ path: "config/config.env" });
 }
 
-app.use(express.json())
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload());
-
-//Route imports
-// import {product}  from "./routes/productRoute.js";
-// import {user} from "./routes/userRoute.js";
-// import {pickup} from "./routes/pickupRoute.js";
-// import {order} from "./routes/orderRoute.js";
-// import {payment} from "./routes/paymentRoute.js";
+// app.use(express.json())
+// app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(fileUpload());
 
 
 //API
 app.use('/api/v1', AppRoutes);
-// app.use("/api/v1",product);
-// app.use("/api/v1",user);
-// app.use("/api/v1",pickup);
-// app.use("/api/v1", order);
-// app.use("/api/v1", payment);
 
 
 //middleware for error
